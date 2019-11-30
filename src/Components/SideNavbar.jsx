fbdd26405384eed90f2f957feab9660873f51800
zabed <zabedalam777@gmail.com>
2019-11-30 01:11:16 +0100

    side nav bar done

--- src/Components/SideNavbar.jsx
+++ src/Components/SideNavbar.jsx
@@ -1,13 +1,64 @@
-import React from "react"
-import {Container} from "reactstrap"
+import React from 'react';
+import logo from '../img/Spotify_Logo.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faSearch, faBookOpen } from '@fortawesome/free-solid-svg-icons';
+
 
 class SideNavbar extends React.Component {
-    state = {  }
-    render() { 
-        return (<>
-        </>
-              );
-    }
+  render() {
+    return (
+      <div className='col-2'>
+        <nav className='navbar navbar-expand-md navbar-white bg-navbar fixed-left justify-content-between'>
+          <div className='nav-container'>
+            <a className='navbar-brand' href='#'>
+              <img src={logo} alt='Spotify_Logo' width='131' height='40' />
+            </a>
+            <button
+              className='navbar-toggler'
+              type='button'
+              data-toggle='collapse'
+              data-target='#navbarNavAltMarkup'
+              aria-controls='navbarNavAltMarkup'
+              aria-expanded='false'
+              aria-label='Toggle navigation'
+            >
+              <span className='navbar-toggler-icon'></span>
+            </button>
+            <div className='collapse navbar-collapse' id='navbarNavAltMarkup'>
+              <div className='navbar-nav'>
+                <ul>
+                  <li>
+                    <a className='nav-item nav-link' href='#'>
+                      <span><FontAwesomeIcon icon={faHome} /></span>&nbsp; Home
+                    </a>
+                  </li>
+                  <li>
+                    <a className='nav-item nav-link' href='#'>
+                      <span><FontAwesomeIcon icon={faSearch} /></span>&nbsp; Search
+                    </a>
+                  </li>
+                  <li>
+                    <a className='nav-item nav-link' href='#'>
+                      <span><FontAwesomeIcon icon={faBookOpen} /></span>&nbsp; Your Library
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+          <div className='nav-btn'>
+            <button className='btn signup-btn' type='button'>
+              Sign Up
+            </button>
+            <button className='btn login-btn' type='button'>
+              Login
+            </button>
+            <a href='#'>Cookie Policy</a> |<a href='#'> Privacy</a>
+          </div>
+        </nav>
+      </div>
+    );
+  }
 }
- 
 export default SideNavbar;
