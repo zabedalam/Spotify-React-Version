7ab54c96e91ddde9d7ebe43ba90389b76fdac3f5
zabed <zabedalam777@gmail.com>
2020-10-14 17:32:46 +0200

    new version first commit

--- src/Components/MainComponent.jsx
+++ src/Components/MainComponent.jsx
@@ -1,20 +1,22 @@
 import React from "react"
 import {Container} from "reactstrap"
-import HomePage from "./HomePage"
-import Navbar from "./Navbar";
-import SideNavbar from "./SideNavbar";
-import Footer from "./Footer";
+// import HomePage from "./HomePage"
+// import Navbar from "./Navbar";
+// import SideNavbar from "./SideNavbar";
+// import Footer from "./Footer";
+import Login from "../Components/login/login"
 
 class MainComponent extends React.Component {
     state = {  }
     render() { 
         return ( 
-            <Container>
-                <Navbar></Navbar>
-                <SideNavbar></SideNavbar>
-            <HomePage></HomePage>
-            <Footer></Footer>
-            </Container>
+             <Container>
+            {/* //     <Navbar></Navbar>
+            //     <SideNavbar></SideNavbar>
+            // <HomePage></HomePage>
+            // <Footer></Footer> */}
+            <Login/>
+             </Container>
          );
     }
 }
---
+++ src/Components/login/login.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./login.css"
+
+export default function Login() {
+  return (
+    <>
+    <div className="login">
+        <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" 
+        alt="spotify logo"/>
+        <a href="">LOGIN TO SPOTIFY</a>
+    </div>
+    </>
+  );
+}
