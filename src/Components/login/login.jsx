d561e93b699eb4f52bf37f72ac5ebe427087750f
zabed <zabedalam777@gmail.com>
2020-10-16 16:05:25 +0200

    login authentication working

--- src/Components/login/login.jsx
+++ src/Components/login/login.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./login.css"
+import {accessUrl} from "../spotify/spotify"
 
 export default function Login() {
   return (
@@ -7,7 +8,7 @@ export default function Login() {
     <div className="login">
         <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" 
         alt="spotify logo"/>
-        <a href="">LOGIN TO SPOTIFY</a>
+        <a href={accessUrl}>LOGIN TO SPOTIFY</a>
     </div>
     </>
   );
