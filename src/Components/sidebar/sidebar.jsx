dc35239ec44cfdd86e5a8ebf33c435297c39b421
zabed <zabedalam777@gmail.com>
2020-10-20 00:16:32 +0200

    include material ui for icons

--- src/Components/sidebar/sidebar.jsx
+++ src/Components/sidebar/sidebar.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import SidebarOption from '../sidebaroption/sidebarOption';
 import "./sidebar.css"
+import HomeIcon from "@material-ui/icons/Home";
+import SearchIcon from "@material-ui/icons/Search";
+import LibraryMusicIcon from "@material-ui/icons/LibraryMusic";
 export default function Sidebar() {
   return (
     <>
     <div className="sidebar">
-    <h1>Hello I am From Sidebar</h1>
+    {/* <h1>Hello I am From Sidebar</h1> */}
+    <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+     alt="spotify logo" className="sidebar__logo"/>
+     <SidebarOption Icon={HomeIcon} title="Home"/>
+     <SidebarOption Icon={SearchIcon} title="Search"/>
+     <SidebarOption Icon={LibraryMusicIcon} title="Your Library"/>
     </div>
     </>
   );
---
+++ src/Components/sidebaroption/sidebarOption.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./sidebarOption.css"
+
+export default function SidebarOption({title,Icon}) {
+  return (
+    <>
+    <div className="sidebarOption">
+  {Icon && <Icon className="sidebarOption__icon"/>}
+  {Icon ? <h6>{title}</h6>:<p>{title}</p>}
+    </div>
+   
+    </>
+  );
+}
