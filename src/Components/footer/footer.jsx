3e8e3ea775177e6157b3a4f46f458d2f31e865f0
zabed <zabedalam777@gmail.com>
2020-10-20 22:18:47 +0200

    footer design done

--- src/Components/footer/footer.jsx
+++ src/Components/footer/footer.jsx
@@ -1,10 +1,47 @@
 import React from 'react';
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import ShuffleIcon from "@material-ui/icons/Shuffle";
+import RepeatIcon from "@material-ui/icons/Repeat";
+import VolumeDownIcon from "@material-ui/icons/VolumeDown";
+import PauseCircleOutlineIcon from "@material-ui/icons/PauseCircleOutline";
+import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay";
+import { Grid, Slider } from "@material-ui/core";
 import "./footer.css"
 export default function Footer() {
   return (
     <>
     <div className="footer">
-    <h1>Hello I am From Footer</h1>
+    <div className="footer__left">
+      <img className="footer__albumLogo"src="https://www.facebook.com/photo?fbid=10156368089534060&set=a.432747709059" alt=""/>
+      <div className="footer__songInfo">
+        <h4>
+          Yeah!
+        </h4>
+        <p>Usher</p>
+      </div>
+    </div>
+    <div className="footer__center">
+      <ShuffleIcon className="footer__green"/>
+      <SkipPreviousIcon className="footer__icon"/>
+      <PlayCircleOutlineIcon fontSize="large" className="footer__icon"/>
+      <SkipNextIcon className="footer__icon"/>
+      <RepeatIcon className="footer__green"/>
+    </div>
+    <div className="footer__right">
+      <Grid container spacing={2}>
+<Grid item>
+  <PlaylistPlayIcon/>
+</Grid>
+<Grid>
+  <VolumeDownIcon/>
+  </Grid> 
+  <Grid>
+    <Slider/>
+    </Grid> 
+      </Grid>
+    </div>
     </div>
     </>
   );
