05df92425e57a75d8cdec39b126121b063d45408
zabed <zabedalam777@gmail.com>
2020-10-21 14:20:40 +0200

    banner design done

--- src/Components/body/body.jsx
+++ src/Components/body/body.jsx
@@ -8,6 +8,16 @@ export default function Body({spotify}) {
       <div className="body">
         {/* <h1>Hello I am From Body</h1> */}
         <Header spotify={spotify}></Header>
+        <div className="body__info">
+        <img src="https://media-exp1.licdn.com/dms/image/C4D03AQGAgxKJNTdFRg/profile-displayphoto-shrink_200_200/0?e=1608768000&v=beta&t=jQsB84R2mIpAt4pnFa1AFp2u-jgj4LpBoH6Seiq9A3Q" 
+        alt="user images"/>
+        <div className="body__infoText">
+          <strong>PLAYLIST</strong>
+          <h2>Discover Weekly</h2>
+          <p>Description....</p>
+        </div>
+        </div>
+       
       </div>
     </>
   );
--- src/Components/header/header.jsx
+++ src/Components/header/header.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import "./header.css";
 import { Avatar } from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
+import {useDataLayerValue} from "../datalayer/datalayer"
 
 export default function Header() {
+    const [{user},dispatch]=useDataLayerValue()
   return (
     <>
       <div className="header">
@@ -18,8 +20,8 @@ export default function Header() {
         {/* <h1>I am from Header</h1> */}
         <div className="header__right">
           {/* <h1>I am from header Right</h1> */}
-          <Avatar src="" alt="Zabedul" />
-          <h4>Zabedul</h4>
+          <Avatar src={user?.images[0]} alt={user?.display_name} />
+  <h4>{user?.display_name}</h4>
         </div>
       </div>
     </>
